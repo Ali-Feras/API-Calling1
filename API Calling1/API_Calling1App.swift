cf80bedbd80e56d9a5f0996a4c6bd930e49cc2f0
Ali Feras <ferasali876@gmail.com>
2022-07-26 14:00:11 -0500

    Initial Commit

---
+++ API Calling1/API_Calling1App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling1App.swift
+//  API Calling1
+//
+//  Created by Ali on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
